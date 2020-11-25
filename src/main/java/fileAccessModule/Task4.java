f1d4f4560f803c77588479f4a602967382e80dbf
kssadomtsev <sadomcev.k@gmail.com>
2020-11-25 19:27:39 +0300

    25.11.2020-2

---
+++ src/main/java/fileAccessModule/Task4.java
@@ -0,0 +1,10 @@
+package main.java.fileAccessModule;
+
+public class Task4 {
+    public static void main(String[] args) {
+        int a = -74;
+        System.out.println(Integer.toBinaryString(a));
+        System.out.println(Integer.toBinaryString(a));
+        System.out.println(a & 0xFF);
+    }
+}
