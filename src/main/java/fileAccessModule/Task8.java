b69bf94d4cc8401974b050c91e6ebfda477d548e
kssadomtsev <sadomcev.k@gmail.com>
2020-11-26 14:30:17 +0300

    Ended IO module

--- src/main/java/fileAccessModule/Task8.java
+++ src/main/java/fileAccessModule/Task8.java
@@ -7,36 +7,41 @@ import java.util.Objects;
 public class Task8 {
 
     public static void main(String[] args) {
-        Animal animal1 = new Animal("A1");
-        Animal animal2 = new Animal("A2");
+        Animal[] animals = {new Animal("A1"), new Animal("A2"), new Animal("A3")};
         byte[] yourBytes = null;
         ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
         try (ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteArrayOutputStream)) {
-            objectOutputStream.writeObject(animal1);
-            objectOutputStream.writeObject(animal2);
-            objectOutputStream.flush();
+            objectOutputStream.writeInt(animals.length);
+            for (Animal animal : animals) {
+                objectOutputStream.writeObject(animal);
+            }
             yourBytes = byteArrayOutputStream.toByteArray();
-
         } catch (IOException e) {
             e.printStackTrace();
         }
-        System.out.println(Arrays.toString(yourBytes));
-        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(yourBytes);
-        try (ObjectInputStream objectInputStream = new ObjectInputStream(byteArrayInputStream)) {
-            Animal animal3 = (Animal) objectInputStream.readObject();
-            Animal animal4 = (Animal) objectInputStream.readObject();
-            System.out.println(animal3);
-            System.out.println(animal4);
-
-        } catch (IOException | ClassNotFoundException e) {
-            e.printStackTrace();
-        }
+        Animal[] animalsOut = deserializeAnimalArray(yourBytes);
+        System.out.println(Arrays.toString(animalsOut));
 
     }
 
-//    public static Animal[] deserializeAnimalArray(byte[] data) {
-//        //Твой код здесь
-//    }
+public static Animal[] deserializeAnimalArray(byte[] data) {
+    if (data == null) {
+        throw new IllegalArgumentException();
+    }
+    Animal[] animals = null;
+    ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(data);
+    try (ObjectInputStream objectInputStream = new ObjectInputStream(byteArrayInputStream)) {
+        int size = objectInputStream.readInt();
+        animals = new Animal[size];
+        for (int i = 0; i < size; i++) {
+            animals[i] = (Animal) objectInputStream.readObject();
+
+        }
+    } catch (Exception e) {
+        throw new IllegalArgumentException();
+    }
+    return animals;
+}
 
     static class Animal implements Serializable {
         private final String name;
